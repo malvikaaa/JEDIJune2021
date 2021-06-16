034a43bac30ad596e48706b114ddc5b654e41491
malvikaaa <malvika2698@gmail.com>
2021-06-16 14:58:06 +0530

    Following changes are done 	new file:   addition.c 		added sum function
    	 helloWorld.c
    		Refactored code to helloFunction

---
+++ code/addition.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+	int a=100;
+	int b=90;
+	printf(a+b);
+}
--- code/helloWorld.c
+++ code/helloWorld.c
@@ -1,4 +1,9 @@
+//hello world program
 #include<stdio.h>
 int main(){
+	helloworld();
+}
+void helloworld(){
 	printf("Hello WOrld");
+	printf("Awesome world");
 }
