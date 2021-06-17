6e010ef8155bae89018fc5fc43becbf7c06bf1bb
malvikaaa <47711463+malvikaaa@users.noreply.github.com>
2021-06-17 11:58:21 +0530

    Update helloWorld.c

--- CCode/helloWorld.c
+++ CCode/helloWorld.c
@@ -6,6 +6,11 @@ void greeting(char name[]);
 int main() {
         char name[] = "Alia Bhat"
         greeting(name);
+        helloWorld();
+}
+void helloWorld()
+{
+        printf("Hello World");
 }
 
 void greeting(char name[]) {
