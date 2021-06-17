7d2cd5592ae16dd375676f1e20411347a2f9355e
malvikaaa <malvika2698@gmail.com>
2021-06-17 11:53:48 +0530

    Changes committed: 	modified:   CCode/helloWorld.c 	modified:   JavaCode/Hello.java

--- JavaCode/Hello.java
+++ JavaCode/Hello.java
@@ -1,11 +1,15 @@
-class HelloWorld {
+package learnJava;
+
+public class Hello {
     public static void main(String[] args) {
         helloWorld();
     }
-	public void helloWorld()
-	{
-		 System.out.println("Hello, World!");
 
-	}
+    public void helloWord() {
+        System.out.println("Hello, World!");
+    }
+
+    public void greeting(String name) {
+        System.out.println("Hey, How are you doing?." +  name);
+    }
 }
-
