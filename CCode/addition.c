290be7a1734d75e169a060b2f780fb918bc80941
malvikaaa <malvika2698@gmail.com>
2021-06-17 10:11:58 +0530

    Changes Committed: 	modified:   CCode/addition.c 		added smmation of 3 arguments 	modified:   JavaCode/Hello.java 		refracted helloworld code

--- CCode/addition.c
+++ CCode/addition.c
@@ -3,6 +3,10 @@ int sum(int x,int y)
 {
 	return (x+y);
 }
+int sum3(int x,int y,int z){
+	return (x+y+z);
+}
+
 int main()
 {
 	return sum(30,78);
--- JavaCode/Hello.java
+++ JavaCode/Hello.java
@@ -1,6 +1,11 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        helloWorld();
     }
+	public void helloWorld()
+	{
+		 System.out.println("Hello, World!");
 
+	}
 }
+
